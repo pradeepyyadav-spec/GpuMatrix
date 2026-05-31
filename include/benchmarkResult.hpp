954a726f65fa672a689a1b72c034f681d630f543
Pradeep Yadav <g25ait1110@iitj.ac.in>
2026-05-31 16:39:46 +0530

    Variable size of matrix

--- include/benchmarkResult.hpp
+++ include/benchmarkResult.hpp
@@ -2,9 +2,8 @@
 
 struct BenchmarkResult
 {
+    int matrixSize = 0;
     double executionTimeSeconds = 0.0;
-
     double speedup = 0.0;
-
     double gflops = 0.0;
 };
--- src/cpu/cpuExponentiationBenchmark.cpp
+++ src/cpu/cpuExponentiationBenchmark.cpp
@@ -1,27 +1,63 @@
 #include "../../include/timer.hpp"
 #include "../../include/denseCpu.hpp"
 #include "../../include/matrixGenerator.hpp"
+#include "../../include/benchmarkResult.hpp"
 
 #include <iostream>
+#include <vector>
+#include <iomanip>
 
 int main()
 {
-    constexpr int matrixSize = 512;
+    constexpr int matrixSizes[] = { 512, 1024, 2048,4096 };
+
     constexpr int exponentValue = 100;
 
-    std::cout << "Generating Matrix..." << std::endl;
+    std::vector<BenchmarkResult> benchmarkResults;
 
-    DenseMatrix matrix = MatrixGenerator::createEdaDenseMatrix( matrixSize );
+    std::cout << "CPU Matrix Exponentiation Benchmark\n" << "Exponent = " << exponentValue << std::endl;
 
-    Timer timer;
+    for (int matrixSize : matrixSizes)
+    {
+        std::cout << "Matrix Size : " << matrixSize << " x " << matrixSize << std::endl;
 
-    timer.start();
+        DenseMatrix matrix = MatrixGenerator::createEdaDenseMatrix( matrixSize );
 
-    DenseMatrix result = DenseCpu::matrixPower( matrix, exponentValue );
+        double matrixMemoryMb = static_cast<double>( matrix.getSizeInBytes() ) / (1024.0 * 1024.0);
 
-    double executionTime = timer.stop();
+        std::cout << "Approx Matrix Memory : " << std::fixed << std::setprecision(2) << matrixMemoryMb << " MB" << std::endl;
 
-    std::cout << "CPU Runtime: " << executionTime << " sec" << std::endl;
+        Timer timer;
+
+        timer.start();
+
+        DenseMatrix result = DenseCpu::matrixPower( matrix, exponentValue );
+
+        double executionTime = timer.stop();
+
+        BenchmarkResult benchmarkResult;
+
+        benchmarkResult.executionTimeSeconds = executionTime;
+
+        benchmarkResults.push_back( benchmarkResult );
+
+        std::cout << "Execution Time : " << executionTime << " sec" << std::endl;
+    }
+
+    std::cout << "Benchmark Summary" << std::endl;
+
+    std::cout << std::left << std::setw(15) << "Matrix Size" << std::setw(20) << "Runtime (sec)" << std::endl;
+
+    std::cout
+        << "---------------------------------------------\n";
+
+    for ( std::size_t index = 0; index < benchmarkResults.size(); index++ )
+    {
+        std::cout << std::left << std::setw(15) << matrixSizes[index] << std::setw(20) << std::fixed << std::setprecision(4) << benchmarkResults[index].executionTimeSeconds << std::endl;
+    }
+
+    std::cout
+        << "---------------------------------------------\n";
 
     return 0;
 }
