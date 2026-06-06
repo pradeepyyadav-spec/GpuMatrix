97f559a3740c90e526f77709e7bec4f555669fb3
Pradeep Yadav <g25ait1110@iitj.ac.in>
2026-06-06 11:54:14 +0530

    Fix bug for 2048 matrix exponentiation

--- src/gpu/gpuExponentiationBenchmark.cu
+++ src/gpu/gpuExponentiationBenchmark.cu
@@ -26,34 +26,29 @@ bool validateResults( const DenseMatrix& cpuResult, const DenseMatrix& gpuResult
 
 int main()
 {
-    constexpr int matrixSize = 2048;
+    constexpr int matrixSize = 1024;
     constexpr int exponentValue = 100;
 
-    DenseMatrix matrix = MatrixGenerator::createEdaDenseMatrix( 1024 );
-    DenseMatrix matrix2048 = MatrixGenerator::createEdaDenseMatrix( matrixSize );
+    DenseMatrix matrix = MatrixGenerator::createEdaDenseMatrix( matrixSize );
 
-
-    Timer cpuTimer;
-    cpuTimer.start();
-
-    DenseMatrix cpuResult = DenseCpu::matrixPower( matrix, exponentValue );
-
-    double cpuTime = cpuTimer.stop();
-
-    Timer gpuTimer;
-    gpuTimer.start();
-
-    DenseMatrix gpuResult = DenseGpu::matrixPowerTiled( matrix, exponentValue );
-
-    double gpuTime = gpuTimer.stop();
-
-    bool validationPassed = validateResults( cpuResult, gpuResult );
-    
     std::cout << "=============================================\n";
     std::cout << "CPU vs GPU Matrix Exponentiation\n";
     std::cout << "Matrix Size : " << matrixSize << " x " << matrixSize << "\n";
     std::cout << "Exponent : " << exponentValue << "\n";
     std::cout << "=============================================\n";
+
+    Timer cpuTimer;
+    cpuTimer.start();
+    DenseMatrix cpuResult = DenseCpu::matrixPower( matrix, exponentValue );
+    double cpuTime = cpuTimer.stop();
+
+    Timer gpuTimer;
+    gpuTimer.start();
+    DenseMatrix gpuResult = DenseGpu::matrixPowerTiled( matrix, exponentValue );
+    double gpuTime = gpuTimer.stop();
+
+    bool validationPassed = validateResults( cpuResult, gpuResult );
+    
     std::cout << "\n=========================================================\n";
     std::cout << std::left << std::setw(20) << "Configuration" << std::setw(15) << "Runtime(sec)" << std::setw(15)
               << "Speedup" << std::setw(15) << "Validation" << "\n";
@@ -65,18 +60,22 @@ int main()
 
     std::cout << "=========================================================\n";
 
-    Timer gpuTimer2048;
-    gpuTimer2048.start();
 
-    DenseMatrix gpuResult2048 = DenseGpu::matrixPowerTiled( matrix2048, exponentValue );
-
-    double gpuTime2048 = gpuTimer2048.stop();
+    // 2048 Matrix now.
+    matrixSize = 2048;
+    DenseMatrix matrix2048 = MatrixGenerator::createEdaDenseMatrix( matrixSize );
 
     std::cout << "\n\n\n\n=============================================\n";
     std::cout << "CPU vs GPU Matrix Exponentiation\n";
-    std::cout << "Matrix Size : " << 2048 << " x " << 2048 << "\n";
+    std::cout << "Matrix Size : " << matrixSize << " x " << matrixSize << "\n";
     std::cout << "Exponent : " << exponentValue << "\n";
     std::cout << "=============================================\n";
+
+    Timer gpuTimer2048;
+    gpuTimer2048.start();
+    DenseMatrix gpuResult2048 = DenseGpu::matrixPowerTiled( matrix2048, exponentValue );
+    double gpuTime2048 = gpuTimer2048.stop();
+
     std::cout << "\n=========================================================\n";
     std::cout << std::left << std::setw(20) << "Configuration" << std::setw(15) << "Runtime(sec)" << std::setw(15)
               << "Speedup" << std::setw(15) << "Validation" << "\n";
